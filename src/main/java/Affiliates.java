c1347fef3e5ef16ec63f3988d54d58fb85554fee
jbasilious <jbasilious@gmail.com>
2025-06-15 12:31:23 -0400

    added affiliate links and youtube rss feed

---
+++ src/main/java/Affiliates.java
@@ -0,0 +1,27 @@
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Links {
+
+    public static void run(MessageReceivedEvent event){
+    }
+
+    public static void tribute(MessageReceivedEvent event) {
+    }
+
+    public static void blauer(MessageReceivedEvent event) {
+    }
+
+    public static void evo(MessageReceivedEvent event) {
+    }
+
+    public static void amazon(MessageReceivedEvent event) {
+        event.getMessage().reply("[Amazon](https://amzn.to/45ODItv)" +
+                "[Evo](https://www.evo.com/)").queue();
+
+
+
+    }
+
+    public static void links(MessageReceivedEvent event) {
+    }
+}
--- src/main/java/Amazon.java
+++
@@ -1,8 +0,0 @@
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-
-public class Amazon {
-
-    public static void run(MessageReceivedEvent event){
-        event.getMessage().reply("https://amzn.to/45ODItv").queue();
-    }
-}
